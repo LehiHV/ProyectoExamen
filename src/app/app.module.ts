e00cfbc830ad1d1b506e84cdc06a3cec6addd755
Cucarachita Cosmica <lehi_espr0@hotmail.com>
2024-04-16 01:11:53 -0600

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
+import { RecaptchaModule } from 'ng-recaptcha';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { HttpClientModule } from '@angular/common/http'; // Importa HttpClientModule
 
@@ -14,6 +14,7 @@ import { AppRoutingModule } from './app-routing.module';
     BrowserModule,
     IonicModule.forRoot(),
     AppRoutingModule,
+    RecaptchaModule,
     HttpClientModule // Agrega HttpClientModule al array de imports
   ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
